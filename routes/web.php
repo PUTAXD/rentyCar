3dde1b171ac0647be5ea5c07ce18d64f46c81aed
Lidan4315 <faiqlidan15@gmail.com>
2024-10-12 19:37:58 +0700

    navbar

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/rentcar', function () {
+    return view('rentcar');
+});
+
+// Route::get('/', function () {
+//     return view('home');
+// });
